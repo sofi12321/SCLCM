825f9ce7359da04aaa47ff4b6576fc1fd901ac92
Sofi Shulepina <95312480+sofi12321@users.noreply.github.com>
2025-01-18 03:16:15 +0300

    Create classifier model

---
+++ src/models/cnn/classifier.py
@@ -0,0 +1,23 @@
+class Classifier(nn.Module):
+    """
+    Classification model for embedding models
+    """
+    def __init__(self,base_model,output_dim,embedding_dim=64):
+        """
+        Prepare the network
+        :param base_model: model returning embedding of shape (batch, channel, embedding_dim)
+        :param output_dim: Number of classes
+        :param embedding_dim: Length of the embedding
+        """
+        super().__init__()
+        self.base_model = base_model
+        self.layer1 = nn.Linear(embedding_dim, embedding_dim//2)
+        self.last_layer = nn.Linear(embedding_dim//2, output_dim)
+
+    def forward(self,x):
+        # Obtain embedding
+        h = self.base_model(x)
+        # Classification layers
+        h = F.relu(self.layer1(h))
+        h = self.last_layer(h)
+        return h
