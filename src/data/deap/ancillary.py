9d6b1ce0964b29f8aeab17a71489226886fe2873
D4ni1a <71354878+D4ni1a@users.noreply.github.com>
2025-05-09 22:27:20 +0300

    Upload deap metadata

---
+++ src/data/deap/ancillary.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def deap_metadata():
+    num_video = 40
+    subj_list = np.array([26, 27, 28, 29, 30, 31])
+    video_list = np.array([0,1,2,3,4,5,6,7])
+    return num_video, subj_list, video_list
