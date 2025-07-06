76cf8034b9dbd069743e739b8f859a562cb0374b
D4ni1a <71354878+D4ni1a@users.noreply.github.com>
2025-07-06 18:09:45 +0300

    Add model size evaluation

---
+++ src/utils/model_size_eval.py
@@ -0,0 +1,26 @@
+from prettytable import PrettyTable
+
+def print_model_size(mdl):
+    print("Model size:", end=" ")
+    torch.save(mdl.state_dict(), "tmp.pt")
+    print("%.2f MB" %(os.path.getsize("tmp.pt")/2**20))
+    # print(os.path.getsize("tmp.pt")/1e6)
+    os.remove('tmp.pt')
+
+
+def count_parameters_detailed(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in dict(model.state_dict()).items():
+        if not ('weight' in name or 'bias' in name):
+            continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params += params
+    print(f"Total Trainable Params: {total_params}")
+    print("More detailed parameters distribution:")
+    print(table)
+    return total_params
+
+def count_parameters_short(model):
+    print(sum(p.numel() for p in model.parameters()))
