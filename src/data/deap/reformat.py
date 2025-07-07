7f3ce1c6d2b7d53a2e229d221cdf4cd12199ac1e
D4ni1a <71354878+D4ni1a@users.noreply.github.com>
2025-07-07 18:06:06 +0300

    Update path in DEAP reformat

--- src/data/deap/reformat.py
+++ src/data/deap/reformat.py
@@ -2,5 +2,5 @@ from data.deap.load_row import load_deap_raw
 
 def reformat_deap(data_path):
     # Reformating is made by loading raw data
-    data, session_labels, pid_video, num_video, subj_list, video_list, channels = load_deap_raw( length=60*128, data_path)
+    data, session_labels, pid_video, num_video, subj_list, video_list, channels = load_deap_raw( length=60*128, general_path=data_path)
     return data, session_labels, pid_video, num_video, subj_list, video_list, channels
